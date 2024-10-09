c9a9405a24f5d2ab9a5c93e6eb71f5e881c42417
AbhiSahane0 <cloudabhi123@gmail.com>
2024-10-09 10:18:24 +0530

    charity contract code complete

--- donate.org/hardhat.config.js
+++ donate.org/hardhat.config.js
@@ -33,6 +33,14 @@ module.exports = {
   },
   solidity: {
     version: "0.8.17",
+    defaultNetwork: "sepolia",
+    networks: {
+      hardhat: {},
+      sapolia: {
+        url: "wss://ethereum-sepolia-rpc.publicnode.com",
+        accounts: [`0x${process.env.PRIVATE_KEY}`],
+      },
+    },
     settings: {
       optimizer: {
         enabled: true,
