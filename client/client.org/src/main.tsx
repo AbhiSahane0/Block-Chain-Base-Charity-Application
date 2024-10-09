ce29b8e3ad6ada88d385fd6dc9aaa677031a3bf0
AbhiSahane0 <cloudabhi123@gmail.com>
2024-10-09 18:23:00 +0530

    testnet setup mad

--- client/client.org/src/main.tsx
+++ client/client.org/src/main.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
+import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
-import { ThirdwebProvider } from "thirdweb/react";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ThirdwebProvider>
-      <App />
+    <ThirdwebProvider activeChain={ChainId.Sepolia}>
+      <Router>
+        <App />
+      </Router>
     </ThirdwebProvider>
   </React.StrictMode>
 );
