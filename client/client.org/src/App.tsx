39e512181a419a1abc83b99371f8b15e65984940
AbhiSahane0 <cloudabhi123@gmail.com>
2024-10-12 01:01:58 +0530

    icon component created

--- client/client.org/src/App.tsx
+++ client/client.org/src/App.tsx
@@ -6,6 +6,11 @@ function App() {
   return (
     <div className="relative sm:8 p-4 bg-[#13131a] min-h-screen flex flex-row">
       <div className="sm:flex hidden mr10 relative">
+        <Sidebar />
+      </div>
+
+      <div className="flex-1 max-sm:w-full mar-w-[1280px] mx-auto sm:pr-5">
+        Navbar
         <Routes>
           <Route path="/" element={<Home />}></Route>
         </Routes>
