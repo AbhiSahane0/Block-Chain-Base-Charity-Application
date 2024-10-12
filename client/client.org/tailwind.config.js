25bff88890d610df5f0bba3d5c2abe2e3e5b09c4
AbhiSahane0 <cloudabhi123@gmail.com>
2024-10-12 12:30:34 +0530

    Navbar and CustomButton components created

--- client/client.org/tailwind.config.js
+++ client/client.org/tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    fontFamily: {
+      epilogue: ["Epilogue", "sans-serif"],
+    },
   },
   plugins: [],
 };
